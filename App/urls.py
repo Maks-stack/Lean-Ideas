13428c248995f0f81fbe5c2204194e4e00f3abef
Maximilian Menschel <maxmenschel5@gmail.com>
2020-11-17 15:18:17 +0100

    Login and stuff.  base url is http://127.0.0.1:8000 to see users go to http://127.0.0.1:8000/admin Admin Login: User: admin password: admin Register works, Login works

--- App/urls.py
+++ App/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
+from App.views import register, index
 from django.contrib import admin
+
+from django.contrib.auth.views import LoginView, LogoutView
+
+from . import views
 
 from App.views import register
 
 urlpatterns = [
 
     path('admin/', admin.site.urls),
-    path('register', register, name='register')
+    path('',index, name='index'),
+    path('register', register, name='register'),
+    path("login", LoginView.as_view(template_name="login.html"),name="login"),
+    path("logout", LogoutView.as_view(),name ="logout")
 ] 
