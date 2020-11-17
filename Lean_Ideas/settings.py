eea74de94fd1f7ce5b189b24d38cb40b76eaff27
Melvin NAMONT <melvin.namont@gmail.com>
2020-11-17 18:04:00 +0100

    added bootstrap4

--- Lean_Ideas/settings.py
+++ Lean_Ideas/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'App',
-    'django.contrib.admin'
+    'django.contrib.admin',
+    'bootstrap4'
 ]
 
 MIDDLEWARE = [
