0057811b354ccf51016739914c3cb7f017577c85
ivyseike9 <ivyseike9@gmail.com>
2020-11-18 21:37:53 +0800

    Add Idea and Comment models

--- App/admin.py
+++ App/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+
 # Register your models here.
+
+from .models import Idea, Comment
+
+admin.site.register(Idea)
+admin.site.register(Comment)
--- App/models.py
+++ App/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db import models
 
 # Create your models here.
+
+class Idea(models.Model):
+    name = models.CharField(max_length=30)
+    content = models.CharField(max_length=500)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    vote = models.IntegerField()
+
+class Comment(models.Model):
+    content = models.CharField(max_length=500)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    idea = models.ForeignKey(Idea, on_delete=models.CASCADE)
+
+
+
+
