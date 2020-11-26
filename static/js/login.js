18662adfa5d638cf40c12d0eefaec7b7eb3bc80f
Melvin NAMONT <melvin.namont@gmail.com>
2020-11-26 23:58:17 +0100

    added plogin placeholders

---
+++ static/js/login.js
@@ -0,0 +1,6 @@
+usernameInput = document.getElementById("id_username");
+usernameInput.placeholder = "Enter your username";
+
+
+passwordInput = document.getElementById("id_password");
+passwordInput.placeholder = "Enter your password";
