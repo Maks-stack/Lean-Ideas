9546f61bc0ec08664d4bc580cd06919d3803bc1a
Melvin NAMONT <melvin.namont@gmail.com>
2020-12-03 14:23:55 +0100

    add comment button to display comment form

---
+++ static/js/ideaDetails.js
@@ -0,0 +1,10 @@
+let commentSection = document.getElementsByClassName("comment-form")[0];
+let addCommentButton = document.getElementById("add-comment");
+
+
+commentSection.style.display = "none";
+
+addCommentButton.addEventListener("click",(event) =>{
+    commentSection.style.display = "block";
+    addCommentButton.style.display = "none";
+});
